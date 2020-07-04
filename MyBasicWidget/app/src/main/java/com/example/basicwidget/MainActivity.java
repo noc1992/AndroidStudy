a5682a6e70efb2129b018273911b4b432d900f7d
noc1992 <noc817@gmail.com>
2020-07-04 17:11:00 +0900

    업데이트 1

---
+++ MyBasicWidget/app/src/main/java/com/example/basicwidget/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.basicwidget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    RadioButton radioButton = (RadioButton) findViewById(R.id.radioButton3);
+    boolean checked = radioButton.isChecked();
+}
